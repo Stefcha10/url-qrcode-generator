cbf166ff72f1bf12010ac91cab982d2b8804eb89
Stefcha10 <stefan1993ilic@gmail.com>
2022-08-25 14:03:38 +0200

    First commit

---
+++ app/Http/Controllers/QRCodeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
+class QRCodeController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $qrcode = base64_encode(QrCode::size($request->input('size'))
+            ->generate($request->input('url')));
+
+        $pdf = Pdf::loadView('qrcode', compact('qrcode'));
+        $pdf->download('qrcode.pdf');
+
+        return $pdf->download('qrcode.pdf');
+    }
+}
---
+++ resources/views/qrcode.blade.php
@@ -0,0 +1,5 @@
+<div style="text-align: center">
+    <h1>Your QRcode</h1>
+    <img src="data:image/png;base64, {!! $qrcode !!}">
+</div>
+
