af74816b98e4925ed488a54cb1475c69eefe87d7
Stefcha10 <stefan1993ilic@gmail.com>
2023-09-05 17:45:40 +0200

    added new image and couple blade fixes

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,70 +1,60 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Url QR Code generator</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="shortcut icon" href="https://code-cache.com/en/wp-content/uploads/2021/09/qr-code.png">
-        <script src="https://cdn.tailwindcss.com"></script>
-
-
-        <!-- Styles -->
-        <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
-        </style>
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-700">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>URL QR Code Generator</title>
+    
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="{{asset('images/qrcode.png')}}">
+    <script src="https://cdn.tailwindcss.com"></script>
+    
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.tailwindcss.com">
+    <style>
+        /* Your custom styles go here */
+    </style>
+</head>
+<body class="antialiased bg-white dark:bg-gray-900">
+    <nav class="bg-gray-200 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800">
         <div class="container flex flex-wrap justify-between items-center mx-auto">
-          <a href="/" class="flex items-center">
-              <img src="https://code-cache.com/en/wp-content/uploads/2021/09/qr-code.png" class="mr-3 h-6 sm:h-9 rounded-lg" alt="Flowbite Logo" />
-              <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Url QR Code generator</span>
-          </a>
-      </nav>
-    <body class="antialiased"> 
-        <div class="grid lg:grid-cols-2 sm:grid-cols-1 gap-2">
-            <div class="mt-10">
-                <div class="text-center text-zinc-700">
-                    <h1 class="text-2xl mb-5 font-bold">Enter your URL below to generate a QR Code</h1>
-                </div>
-                <form class="flex flex-col" method="post" action="{{ route('generate.qr.code' )}}">
-                    @csrf
-                    <div class="relative z-0 mt-12 w-96 m-auto group">
-                        <input type="url" name="url" id="url" class="peer ... text-zinc-700 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-nonea dark:border-gray-600 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
-                        <label for="url" class="peer-focus:font-medium absolute text-sm text-red-500 dark:text-red-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-red-600 peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">URL</label>
-                        <label for="size" class="block mb-2 mt-8 text-sm font-medium text-red-900 dark:text-red-400">SELECT SIZE OF QRCODE</label>
-                        <select id="size" name="size" required class="bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option value="100">100x100</option>
-                            <option selected value="200">200x200</option>
-                            <option value="300">300x300</option>
-                            <option value="400">400x400</option>
-                            <option value="500">500x500</option>
-                        </select>
-                        <label for="style" class="block mb-2 mt-8 text-sm font-medium text-red-900 dark:text-red-400">SELECT STYLE OF QRCODE</label>
-                        <select id="style" name="style" required class="bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option selected value="square">Square</option>
-                            <option value="round">Round</option>
-                            <option value="dot">Dot</option>
-                        </select>
-                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm mt-3 float-right px-3 py-2 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-800">generate</button>
-                    </div>
-                  </form>
-            </div>
-            <!-- ... -->
-            <div>
-                <img class="rounded" src="https://code-cache.com/en/wp-content/uploads/2021/09/qr-code.png" alt="" srcset="">
-            </div>
-          </div>
-        <div>
+            <a href="/" class="flex items-center">
+                <img src="{{asset('images/qrcode.png')}}" class="mr-3 h-6 sm:h-9 rounded-lg" alt="Flowbite Logo">
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">URL QR Code Generator</span>
+            </a>
         </div>
-    </body>
+    </nav>
+
+    <div class="flex flex-col-reverse 2xl:grid 2xl:grid-cols-12">
+        <div class="mt-10 2xl:col-span-7 flex flex-col">       
+            <h1 class="text-2xl mb-5 dark:text-white text-center font-bold">Enter your URL below to generate a QR Code</h1> 
+            <form class="flex flex-col" method="post" action="{{ route('generate.qr.code' )}}">
+                @csrf
+                <div class="relative z-0 mt-12 w-96 m-auto group">
+                    <input type="url" name="url" id="url" class="dark:text-white peer ... text-zinc-700 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-400 appearance-none dark:border-gray-200 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-500 peer" placeholder="" required>
+                    <label for="url" class="peer-focus font-medium absolute text-sm text-red-900 dark:text-red-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-red-600 peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">ENTER URL</label>
+                    <label for="size" class="block mb-2 mt-8 text-sm font-medium text-red-900 dark:text-red-400">SELECT SIZE OF QR CODE</label>
+                    <select id="size" name="size" required class="dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-300 peer bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-400 dark:placeholder-gray-400 dark:text-white">
+                        <option value="100">100x100</option>
+                        <option selected value="200">200x200</option>
+                        <option value="300">300x300</option>
+                        <option value="400">400x400</option>
+                        <option value="500">500x500</option>
+                    </select>
+                    <label for="style" class="block mb-2 mt-8 text-sm font-medium text-red-900 dark:text-red-400">SELECT STYLE OF QR CODE</label>
+                    <select id="style" name="style" required class="dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-300 bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-400 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option selected value="square">Square</option>
+                        <option value="round">Round</option>
+                        <option value="dot">Dot</option>
+                    </select>
+                    <button type="submit" class="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-2 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm mt-10 float-right px-3 py-2 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-800">Generate</button>
+                </div>
+            </form>
+        </div>
+        <div class="2xl:col-span-5 flex mt-10 flex-col">
+            <img class="max-w-fit" src="{{asset('images/qrcode.png')}}" alt="">
+        </div>
+    </div>
+</body>
 </html>
